6aac4f862c4b9f5aa398449aa7dfd95cc6caa6e2
Ntombizilee <beingprivy@gmail.com>
2024-08-23 21:40:51 +0200

    created my weather app using react and api.openweathermap

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Footer() {
+    return (
+      <footer>
+        This project was coded by("") 
+        <a href="https://github.com/Ntombizilee/week4-react" className="GithubLink" target="_blank" rel="noreferrer">
+          Ntombizile Mabuso
+        </a>
+        and hosted on {""}
+        <a href="https://weather-react-app-challenge.netlify.app/" target="_blank" rel="noreferrer">
+          <strong>Netlify</strong>
+          </a>
+        </footer>
+    );
+}
+      
