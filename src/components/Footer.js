5de86d3e02d6ad835b5794799061bebc6de2591f
Ntombizilee <beingprivy@gmail.com>
2024-08-23 23:05:08 +0200

    Included netlify.toml and _redirects to my code in order to get my netlify url to work.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,31 @@ import React from "react";
 export default function Footer() {
     return (
       <footer>
-        This project was coded by("") 
-        <a href="https://github.com/Ntombizilee/week4-react" className="GithubLink" target="_blank" rel="noreferrer">
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Ntombizilee/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Ntombizile Mabuso
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Ntombizilee/week4-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on GitHub{" "}
         </a>
-        and hosted on {""}
-        <a href="https://weather-react-app-challenge.netlify.app/" target="_blank" rel="noreferrer">
-          <strong>Netlify</strong>
-          </a>
-        </footer>
+        and{" "}
+        <a
+          href="https://66c8f5e5f02c1b161b16b847--sparkly-centaur-7dca6e.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     );
 }
       
